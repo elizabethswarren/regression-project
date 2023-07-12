ab2cf2609a2aa917e8035857021cdca0ac148fde
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-07-12 16:24:18 -0500

    Update explore.py and initial report

--- explore.py
+++ explore.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import explained_variance_score
+
 
 
 ##############################################   EXPLORE     ##############################################
@@ -35,3 +40,109 @@ def plot_categorical_and_continuous_vars(df, cat_var_col, con_var_col):
     
     sns.swarmplot(ax=axs[2], x=cat_var_col, y=con_var_col, data=df, s=1)
     axs[2].set_title('swarmplot')
+
+
+
+    ##############################################   MODEL     ##############################################
+
+def ols_lasso_tweedie(X_train, X_validate, y_train, y_validate, metric_df):
+    ''' This function'''
+
+    # make and fit OLS model
+    lm = LinearRegression()
+
+    OLSmodel = lm.fit(X_train, y_train.value)
+
+    # make a prediction and save it to the y_train
+    y_train['value_pred_ols'] = lm.predict(X_train)
+
+    #evaluate RMSE
+    rmse_train_ols = mean_squared_error(y_train.value, y_train.value_pred_ols) ** .5
+
+    # predict validate
+    y_validate['value_pred_ols'] = lm.predict(X_validate)
+
+    # evaluate RMSE for validate
+    rmse_validate_ols = mean_squared_error(y_validate.value, y_validate.value_pred_ols) ** .5
+
+    #append metric
+    metric_df = metric_df.append({
+        'model': 'ols',
+        'RMSE_train': rmse_train_ols,
+        'RMSE_validate': rmse_validate_ols,
+        'R2_validate': explained_variance_score(y_validate.value, y_validate.value_pred_ols)    
+    }, ignore_index=True)
+
+    print(f"""RMSE for OLS using LinearRegression
+        Training/In-Sample:  {rmse_train_ols} 
+        Validation/Out-of-Sample: {rmse_validate_ols}""")
+
+
+    
+    # make and fit OLS model
+    lars = LassoLars(alpha=0.03)
+
+    Larsmodel = lars.fit(X_train, y_train.value)
+
+    # make a prediction and save it to the y_train
+    y_train['value_pred_lars'] = lars.predict(X_train)
+
+    #evaluate RMSE
+    rmse_train_lars = mean_squared_error(y_train.value, y_train.value_pred_lars) ** .5
+
+    # predict validate
+    y_validate['value_pred_lars'] = lars.predict(X_validate)
+
+    # evaluate RMSE for validate
+    rmse_validate_lars = mean_squared_error(y_validate.value, y_validate.value_pred_lars) ** .5
+
+    #append metric
+    metric_df = metric_df.append({
+        'model': 'lasso_alpha0.03',
+        'RMSE_train': rmse_train_lars,
+        'RMSE_validate': rmse_validate_lars,
+        'R2_validate': explained_variance_score(y_validate.value, y_validate.value_pred_lars)    
+    }, ignore_index=True)
+
+    print(f"""RMSE for LassoLars
+        Training/In-Sample:  {rmse_train_lars} 
+        Validation/Out-of-Sample: {rmse_validate_lars}""")
+
+
+    # make and fit OLS model
+    tr = TweedieRegressor(power=1, alpha=1.0)
+
+    Tweediemodel = tr.fit(X_train, y_train.value)
+
+    # make a prediction and save it to the y_train
+    y_train['value_pred_tweedie'] = tr.predict(X_train)
+
+    #evaluate RMSE
+    rmse_train_tweedie = mean_squared_error(y_train.value, y_train.value_pred_tweedie) ** .5
+
+    # predict validate
+    y_validate['value_pred_tweedie'] = tr.predict(X_validate)
+
+    # evaluate RMSE for validate
+    rmse_validate_tweedie = mean_squared_error(y_validate.value, y_validate.value_pred_tweedie) ** .5
+
+    # append metric
+    metric_df = metric_df.append({
+        'model': 'tweedie_power1_alpha1.0',
+        'RMSE_train': rmse_train_tweedie,
+        'RMSE_validate': rmse_validate_tweedie,
+        'R2_validate': explained_variance_score(y_validate.value, y_validate.value_pred_tweedie)    
+    }, ignore_index=True)
+
+    print(f"""RMSE for TweedieRegressor
+        Training/In-Sample:  {rmse_train_tweedie} 
+        Validation/Out-of-Sample: {rmse_validate_tweedie}""")
+
+    return y_train, y_validate, metric_df
+
+
+    
+
+
+
+
